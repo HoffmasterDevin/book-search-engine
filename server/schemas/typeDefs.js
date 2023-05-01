a7228dc07ba4b865338aa92e5e1e9c1fff12f0aa
HoffmasterDevin <118146567+HoffmasterDevin@users.noreply.github.com>
2023-05-01 18:55:46 -0500

    worked on mutations, typedefs, and resolvers

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,1 +1,44 @@
 const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+    type User {
+        _id: ID
+        username: String
+        email: String
+        bookCount: Int
+        password: String
+        savedBooks
+    }
+    input BookInput {
+        bookId: ID
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+    }
+    type Book {
+        _id: ID
+        authors: [String]
+        bookId: String
+        image: String
+        link: String
+        title: String
+    }
+    type Auth {
+        token: ID!
+        user: User
+    }
+    type Query {
+        users: [User]
+        me: User
+    }
+    Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(email: String!, password: String!, username: String!): Auth
+        saveBook(book: BookInput): User
+        removeBook(bookId: ID!): User
+    }
+`;
+
+module.exports = typeDefs;
---
+++ server/utils/mutations.js
@@ -0,0 +1,25 @@
+export const LOGIN_USER = gql`
+    mutation login($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`
+
+export const ADD_USER = gql`
+    mutation addUser($username: String!, email: $email, password: $password) {  
+    
+    }
+`
+
+export const SAVE_BOOK = gql`
+    mutation saveBook($)
+`
+
+export const REMOVE_BOOK = gql`
+    mutation removeBook($)
+`
